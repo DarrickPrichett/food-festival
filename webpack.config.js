9a55c4503080f1a406f850fc05a2ce94120f3f95
DarrickPrichett <darrickprichett@gmail.com>
2021-12-19 17:30:33 -0600

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,11 @@ module.exports = {
         filename: "[name].bundle.js",
         path: __dirname + "/dist",
     },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, ".")
+        }
+    },
     module: {
         rules: [
             {
